b7b509229be3486ebc0aac56b5365ca9bfd947cc
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-09-15 16:35:48 -0400

    Add components folder with Calculator.js

---
+++ components/Calculator.js
@@ -0,0 +1,57 @@
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='calculator'>
+        <div className='screen'>
+        </div>
+        <div className='button-row-1'>
+          < Button iText='AC' type='grey' />
+          < Button iText='+/-' type='grey' />
+          < Button iText='%' type='grey' />
+          < Button iText='/' type='orange' />
+        </div>
+        <div className='button-row-2'>
+          < Button iText='7' type='grey' />
+          < Button iText='8' type='grey' />
+          < Button iText='9' type='grey' />
+          < Button iText='X' type='orange' />
+        </div>
+        <div className='button-row-3'>
+          < Button iText='4' type='grey' />
+          < Button iText='5' type='grey' />
+          < Button iText='6' type='grey' />
+          < Button iText='-' type='orange' />
+        </div>
+        <div className='button-row-4'>
+          < Button iText='1' type='grey' />
+          < Button iText='2' type='grey' />
+          < Button iText='3' type='grey' />
+          < Button iText='+' type='orange' />
+        </div>
+        <div className='button-row-5'>
+          < Button iText='0' type='grey-large' />
+          < Button iText='.' type='grey' />
+          < Button iText='=' type='orange' />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.iText = props.iText;
+    this.type = props.type;
+  }
+
+  render() {
+    return (
+      <button className={this.type}>{this.iText}</button>
+    );
+  }
+}
